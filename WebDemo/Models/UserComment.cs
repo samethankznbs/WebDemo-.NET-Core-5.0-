7d7ac37a92e2c74bcd31289d89fd26b0b52d2988
Samethan <samethankznbs@gmail.com>
2023-10-11 16:56:54 +0300

    v32

---
+++ WebDemo/Models/UserComment.cs
@@ -0,0 +1,9 @@
+﻿namespace WebDemo.Models
+{
+	public class UserComment
+	{
+        public int ID { get; set; }
+
+        public string Username  { get; set; }
+    }
+}
---
+++ WebDemo/ViewComponents/CommentList.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Collections.Generic;
+using WebDemo.Models;
+
+namespace WebDemo.ViewComponents
+{
+	public class CommentList : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			var commentvalues = new List<UserComment>
+			{
+				new UserComment
+				{
+				ID=1,
+				Username="Samethan"
+				},
+				new UserComment {
+				ID=2,
+				Username="Deneme"
+
+				}, new UserComment
+				{
+					ID =3,
+					Username="DAŞLFJ"
+				}
+			};
+			return View(commentvalues);
+
+		}
+	}
+}
